411c7a628b1c43fba4ca6e9fcfa244c0fd014f35
PolDog <manankov.pavel@gmail.com>
2025-02-19 21:00:22 +0300

    Create HW 10

---
+++ HomeWork10/routes/console.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Jobs\ClearCache;
+use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Artisan;
+
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->purpose('Display an inspiring quote');
+
+Schedule::job(ClearCache::class)->hourly();
+// Schedule::job(ClearCache::class)->everyMinute();
